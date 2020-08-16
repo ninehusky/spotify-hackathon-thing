548a988b90ef370d1e45d60cb00ef8d236a7ad67
Vidhaat Dunna <dunna.vidhaat@gmail.com>
2020-08-15 17:11:19 -0700

    adds working spotify login, but not connected to anything super exciting yet

--- spAuth.py
+++ spAuth.py
@@ -1,12 +1,9 @@
 """
-code based off of https://github.com/vanortg/Flask-Spotify-Auth
+some code based off of https://github.com/vanortg/Flask-Spotify-Auth
 """
 
-
-from flask import Flask, redirect
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
+from flask import Flask, redirect, request
+import requests
 
 app = Flask(__name__)
 
@@ -15,6 +12,7 @@ SPOTIFY_URL_TOKEN = "https://accounts.spotify.com/api/token"
 RESPONSE_TYPE = "code"
 SCOPE = "streaming"  # change this to whatever is required
 CLIENT_ID = "1615209a464c4661b5fb11029a5d7b5b"
+CLIENT_SECRET = "6bc9717897a8400f98c624d8e31139d1"
 
 
 def authQuery(redirect_uri):
@@ -22,8 +20,11 @@ def authQuery(redirect_uri):
 
 
 """
-def getUser():
-    return getAuth(CLIENT_ID, RESPONSE_TYPE, "https://localhost/5000/", SCOPE)
+def tokenQuery(redirect_uri):
+    body = {
+
+    }
+    return SPOTIFY_URL_TOKEN + "client_id={}&client_secret={}&grant_type={}&code={}&redirect_uri".format(CLIENT_ID, CLIENT_SECRET, redirect_uri, SCOPE)
 """
 
 
@@ -32,14 +33,29 @@ def hello_world():
     return 'Hello, world'
 
 
-@app.route('/auth/', methods=['GET', 'POST'])
+@app.route('/auth/', methods=["GET", "POST"])
 def authenticate():
     return redirect(authQuery("http://localhost:5000/authLog/"))
 
 
-@app.route('/authLog/')
+@app.route('/authLog/', methods=["GET", "POST"])
 def authLog():
-    return redirect("/authFail")
+    code = request.args.get('code', type = str)
+
+    if "error" in code:
+        return redirect("/authFail")
+
+    body = {"grant_type": "authorization code",
+            "code": code,
+            "redirect_uri": "http://localhost:5000/authLog",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+            }
+
+    #  requests.post(SPOTIFY_URL_TOKEN, grant_type="authorization_code", , redirect_uri="http://localhost:5000/authLog", client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    requests.post(SPOTIFY_URL_TOKEN, data=body)
+
+    return redirect("/authSuccess")
 
 
 @app.route('/authSuccess/')
